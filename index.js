0b5e5b0bcc8d544b951df96917c0801fc383bc84
Emmanuele-D <emmanuele.durante@arkadiagroup.it>
2021-11-07 12:08:36 +0100

    finito

--- index.js
+++ index.js
@@ -1,53 +1,35 @@
-const button = document.getElementById('button');
+const buttonConvert = document.getElementById('button-convert');
+const buttonInvert = document.getElementById('button-invert')
 const fromValue = document.getElementById('from-value');
 const fromCurrency = document.getElementById('from-currency');
 const toValue = document.getElementById("to-value");
 const toCurrency = document.getElementById("to-currency")
 
-fromValue.addEventListener("change", () => {
+buttonConvert.addEventListener("click", () => {
   try{
-    axios.get(`https://api.currencyscoop.com/v1/convert?api_key=4ad56a844f9482003825704998e9cf5d&from=${fromCurrency.value}&to=${toCurrency.value}&amount=${fromValue.value}`)
-    .then( async res => {
-      let response = await res.data.response.value
-      toValue.value = response
+    if(fromValue.value < 0){
+      alert("insert positive values please")
+    } else {
+      axios.get(`https://api.currencyscoop.com/v1/convert?api_key=4ad56a844f9482003825704998e9cf5d&from=${fromCurrency.value}&to=${toCurrency.value}&amount=${fromValue.value}`)
+      .then( async res => {
+        let resultValue = await res.data.response.value
+        toValue.value = resultValue.toFixed(2)
     })
-  } catch (error) {
+    }
+  } catch (error){
     console.error(error)
   }
 })
 
-fromCurrency.addEventListener("change", () => {
-  try{
-    axios.get(`https://api.currencyscoop.com/v1/convert?api_key=4ad56a844f9482003825704998e9cf5d&from=${fromCurrency.value}&to=${toCurrency.value}&amount=${fromValue.value}`)
-    .then( async res => {
-      let response = await res.data.response.value
-      toValue.value = response
-    })
-  } catch (error) {
-    console.error(error)
-  }
-})
+buttonInvert.addEventListener("click", () => {
+  let tempCurrency = fromCurrency.value;
+  let tempValue = fromValue.value;
 
-toValue.addEventListener("change", () => {
-  try{
-    axios.get(`https://api.currencyscoop.com/v1/convert?api_key=4ad56a844f9482003825704998e9cf5d&from=${toCurrency.value}&to=${fromCurrency.value}&amount=${toValue.value}`)
-    .then( async res => {
-      let response = await res.data.response.value
-      fromValue.value = response
-    })
-  } catch (error) {
-    console.error(error)
-  }
-})
-
-toCurrency.addEventListener("change", () => {
-  try{
-    axios.get(`https://api.currencyscoop.com/v1/convert?api_key=4ad56a844f9482003825704998e9cf5d&from=${toCurrency.value}&to=${fromCurrency.value}&amount=${toValue.value}`)
-    .then( async res => {
-      let response = await res.data.response.value
-      fromValue.value = response
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  console.log(tempCurrency)
+  console.log(tempValue)
+  
+  fromCurrency.value = toCurrency.value;
+  fromValue.value = toValue;
+  toCurrency.value = tempCurrency;
+  toValue.value = tempValue.toFixed(2);
 })
